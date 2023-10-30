7ce29766bb606c0ebabc70580a7e086d8f282c83
MalenkovDN <m18dm.dm@gmail.com>
2023-10-30 12:40:42 +0300

    feat: add styles for play component

--- src/components/Play.tsx
+++ src/components/Play.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import style from '../styles/components/Main.module.scss';
+import { Button } from '@mui/material';
+import style from '../styles/components/Play.module.scss'
 
-const Play = () => {
+const Play: React.FC<any> = () => {
     return (
-        <div className={style.main}>
-            <section>
-                <div>
-                    <h1 className={style.main__title}>Твой QUIZ</h1>
-                </div>
-                <div>
-                    <h5>Google was founded in what year</h5>
-                    <div>
+        <section className={style.section}>
+            <h1 className={style.play__title}>Твой QUIZ</h1>
+            <div className={style.questions}> 
+                <div className={style.questions__container}>
+                    <h5 className={style.questions__title}>Google was founded in what year</h5>
+                    <div className={style.questions__main}>
                         <p>1997</p>
                         <p>1998</p>
                     </div>
-                    <div>
+                    <div className={style.questions__main}>
                         <p>1997</p>
                         <p>1998</p>
                     </div>
                 </div>
-            </section>
-        </div>
+                <div>
+                    <Button variant='contained'>Предыдущий</Button>
+                    <Button variant='contained'>Следующий</Button>
+                    <Button variant='contained'>Выход</Button>
+                </div>
+            </div>
+        </section>
     )
 }
 
